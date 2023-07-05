887c5bb4f0ee6e88fad7515efa27a7b9be03e011
Nick Dynin <dynin.nikolaj2010@yandex.ru>
2023-07-05 16:44:41 +0400

    update login logout and transition beetween pages

--- to_do_list/main/models.py
+++ to_do_list/main/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class ToDoList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="todolist", null=True)
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
+
 
 class Item(models.Model):
     todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
@@ -14,4 +18,3 @@ class Item(models.Model):
 
     def __str__(self):
         return self.text
-
